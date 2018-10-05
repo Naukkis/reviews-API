8c16f117526f9438bc7670e6aca9f9c583fbea2a
Naukkis <naukkis@gmail.com>
2018-10-05 19:19:09 +0300

    init api, launch nukes

---
+++ db.js
@@ -0,0 +1,14 @@
+var promise = require('bluebird');
+const monitor = require('pg-monitor');
+
+var options = {
+    promiseLib: promise
+};
+
+monitor.attach(options, ['query', 'error']);
+
+var pgp = require('pg-promise')(options);
+var connectionString = DATABASE_URL;
+var db = pgp(connectionString);
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express     = require('express');
+const path        = require('path');
+const bodyParser  = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.set('port', process.env.PORT || 3002);
+
+app.use(express.static('public'));
+
+app.get('/reviews/', (req, res) => {
+    console.log("oujeaa");
+    res.status(200);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+});
